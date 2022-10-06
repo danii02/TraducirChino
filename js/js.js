fb48a316dbd89c80a6de1579c08dc540fc0d4036
danii02 <josed.loptar@educa.jcyl.es>
2022-10-06 13:51:22 +0200

    Debugger

--- js/js.js
+++ js/js.js
@@ -1,7 +1,19 @@
 const dato = document.getElementById("dato");
+const footer = document.getElementsByTagName("footer")[0];
 
 dato.onkeydown = ev => {
     if (ev.key === ' ' || ev.key.length !== 1) return;
     ev.preventDefault();
-    ev.target.value += String.fromCharCode(parseInt(ev.key.toUpperCase().charCodeAt(0) + "e8",16));
+    
+    let upperCase = ev.key.toUpperCase();
+    if (/[^A-Z0-9]/g.test(upperCase)) return;
+
+    let value = String.fromCharCode(parseInt(upperCase.charCodeAt(0) + "e8",16));
+    ev.target.value += value;
+    footer.innerHTML += "<p>tipo=" + ev.type + " key=" + ev.key + " fromCharCode="+value+"</p>";
+}
+
+function reiniciar() {
+    dato.value = "";
+    footer.innerHTML = "";
 }
